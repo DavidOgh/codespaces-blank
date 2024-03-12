31ca0fb17d122aaa99fb90a97336574de395a2c5
DavidOgh <160128626+DavidOgh@users.noreply.github.com>
2024-03-12 17:02:23 +0000

    fixing middelware

--- src/routes/weatherRoute.ts
+++ src/routes/weatherRoute.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { getWeatherData } from "../controllers/weatherController.js";
-import { validateCityName } from "../middleware/validator.js";
+import { validateCityName } from "../middleware/validators.js";
 
 const router =  express.Router();
 
